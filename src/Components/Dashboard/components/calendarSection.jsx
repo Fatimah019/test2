8c50248d79b49e2bbdda85bcb3d9ad81f38804b5
Davies Fatimah Omowonuola <daviesfatimah97@gmail.com>
2021-12-16 07:35:47 +0000

    .

--- src/Components/Dashboard/components/calendarSection.jsx
+++ src/Components/Dashboard/components/calendarSection.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import DatePicker from "sassy-datepicker";
-// import "./index.css";
 
 const scheduleData = [
   {
