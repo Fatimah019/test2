00f0a0c5a8cd5d09b18b6b0607d82aaea24108cf
Davies Fatimah Omowonuola <daviesfatimah97@gmail.com>
2021-12-16 09:06:57 +0000

    responsive nav bar

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import Search from "../Search";
 
 const Header = () => {
+  const [showProfile, setShowProfile] = useState(false);
+
+  const handleShowProfile = () => {
+    setShowProfile(true);
+  };
+  const handleCloseProfile = () => {
+    setShowProfile(false);
+  };
+
+  const profileStyle = showProfile ? { display: "flex" } : {};
   return (
     <header>
       <h1 role="img">
         Sport <br />
         Time
       </h1>
-      <div className="header-right">
-        <p className="date">Apiril 18, 2021</p>
-        <div className="search-box">
-          <Search />
-        </div>
-        <div className="user-profile">
-          <div className="user">
-            <p className="user-name">Eleanor Pena</p>
-            <p className="user-title">Admin</p>
+      <nav className="nav" style={profileStyle}>
+        <div className="header-right">
+          <p className="date">Apiril 18, 2021</p>
+          <div className="search-box">
+            <Search />
           </div>
-          <img src="/assets/images/profile-img.png" alt="user" />
+          <div className="user-profile">
+            <div className="user">
+              <p className="user-name">Eleanor Pena</p>
+              <p className="user-title">Admin</p>
+            </div>
+            <img src="/assets/images/profile-img.png" alt="user" />
+          </div>
         </div>
-      </div>
-      <i className="fa fa-bars"></i>
+      </nav>
+      {!showProfile ? (
+        <i className="fa fa-bars" onClick={handleShowProfile}></i>
+      ) : (
+        <i className="fa fa-close" onClick={handleCloseProfile}></i>
+      )}
     </header>
   );
 };
